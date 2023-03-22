c803747085d116502da9e52aee72d237913d8c0f
Boulaajoul-Anass <boulaajoul.anass@gmail.com>
2023-03-22 17:03:26 +0000

    test

---
+++ app/Models/Etudiant.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class Etudiant extends Model
+{
+    use HasFactory, Notifiable, HasApiTokens;
+
+   protected $fillable = ['nom', 'prenom', 'email', 'password', 'filiere_id'];
+
+    public function filiere()
+    {
+        return $this->belongsTo(Filiere::class);
+    }
+
+    public function examens()
+    {
+        return $this->belongsToMany(Examen::class)->withPivot('valeur');
+    }
+
+    public function notes()
+    {
+        return $this->hasMany(Note::class);
+    }
+}
